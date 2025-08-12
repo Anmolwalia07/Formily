d1d941a041423175c37e88bc29c33104f9d1aa11
Anmol walia <anmolwalia025@gmail.com>
2025-08-13 00:44:15 +0530

    addclientenv

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:process.env.CLIENT,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
 
